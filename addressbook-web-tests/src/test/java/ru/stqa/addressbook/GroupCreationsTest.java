8236589d6509ab277a66271f1154cce229034c3b
Evgeny Telepnev <58485098+telepnev@users.noreply.github.com>
2020-09-05 19:20:23 +0300

    Фикстуры

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/GroupCreationsTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/GroupCreationsTest.java
@@ -14,10 +14,7 @@ public class GroupCreationsTest {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
 
-  @Test
-  public void testGroupCreations() throws Exception {
     wd.get("http://localhost/addressbook/index.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -25,6 +22,11 @@ public class GroupCreationsTest {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreations() throws Exception {
+
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
