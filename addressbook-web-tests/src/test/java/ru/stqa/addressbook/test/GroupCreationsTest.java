a210607bdfda9c7872b358810c1e0544f2cad6d6
Evgeny Telepnev <telepnef@bk.ru>
2020-09-16 09:26:06 +0300

    Add Fluent-интерфейсы

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/test/GroupCreationsTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/test/GroupCreationsTest.java
@@ -16,12 +16,12 @@ public class GroupCreationsTest extends TestBase {
 
     app.goTo().groupPage();
     List<GroupData> before = app.group().list();
-    GroupData group = new GroupData("TestNew", null, null);
+    GroupData group = new GroupData().withName("Test");
     app.group().create(group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    group.withId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
     before.sort(byId);
--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/test/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/test/GroupModificationTest.java
@@ -15,7 +15,7 @@ public class GroupModificationTest extends TestBase {
   public void ensurePreconditions() {
     app.goTo().groupPage();
     if (app.group().list().size() == 0) {
-      app.group().create(new GroupData("Test1", null, null));
+      app.group().create(new GroupData().withName("TestNew"));
     }
   }
 
@@ -24,8 +24,8 @@ public class GroupModificationTest extends TestBase {
 
     List<GroupData> before = app.group().list();
     int index = before.size() - 1;
-    GroupData group = new GroupData(before.get(index).getId(),
-            "New Test", null, null);
+    GroupData group = new GroupData().withId(before.get(index).getId()).
+            withName("Test").withHeader("TestToHeader").withFooter("TestToFooter");
     app.group().modify(index, group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size());
